1f3a50cd8483ab6802dd4bf367420ba72e66e3c3
ShauryaSingh-git <talktoshourya@gmail.com>
2025-08-09 00:02:36 +0530

    Difficulty-Easy

--- Arrays/Concatination-of-Array/Concatination-of-Array.cpp
+++ Arrays/Concatination-of-Array/Concatination-of-Array.cpp
@@ -1,22 +1,4 @@
-/*
-Problem number-1929 Leetcode
-This problem is very basic , that is why it is our first array problem
-let the array/vector be [1,2,1] and its size n = 3 which is generally the number of elements inside the array
 
-This problems just wants us to produce its concatination which is it wants the same elememts copied or concatinated behind the original elements few examples-
-arr=[1,2,1] size n=3     concatination = arr=[1,2,1,1,2,1] and size N=2n=6
-arr=[3,6,7,4,11] size n=5     concatination = arr=[3,6,7,4,11,3,6,7,4,11] and size N=2n=6
-
-PREREQUISITES-
-1. Concepts of vector
-2. Loops
-
-TIME COMPLEXITY- 0(n)
-1. first for loop - 0(n)
-2. second for loop - 0(n)
-total= n+n= 2n {we dont count constants} , so time complexity =O(n)
-
-*/
 
 
 #include <iostream>
